cc2cf40be26bb5eab3c88aa527ba5915a64e2c49
Gaurav Singh <gaurav2115402-d@akgec.ac.in>
2026-05-21 10:23:09 +0530

    script-variables-practice

---
+++ javascript-beginner-practice-questions/script-variables.js
@@ -0,0 +1,47 @@
+
+// Create a variable called studentName and store your name in it.
+var studentName="Gaurav Singh";
+
+
+// Create a variable age and print it.
+var age = 27;
+console.log(age);
+
+
+// Create two variables and swap their values.
+let a = 5;
+let b = 7;
+// 1. Pour a into the temporary glass
+let temp = a;
+// 2. Now that glass 'a' is empty, pour b into a
+a = b,
+// 3. Pour the temporary glass into b
+b = temp;
+console.log(a);
+console.log(b);
+
+
+// Create a constant variable for PI and print it.
+const pi = "PI";
+console.log(pi);
+
+
+// Declare a variable without assigning a value and print it.
+var c;
+console.log(c);
+
+
+// Create a variable score and increase it by 10.
+var score=0;
+score += 10;
+console.log(score);
+
+
+// Create three variables for first name, last name, and full name.
+let fname = "Gaurav";
+let lname = "Singh";
+let full_name = "Gaurav Singh";
+console.log(fname + "\n" + lname + "\n" + full_name);
+
+
+
