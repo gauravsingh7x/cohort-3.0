8acf0b54b7c7878eb702c32bff6e8b460cd23497
Gaurav Singh <gaurav2115402-d@akgec.ac.in>
2026-05-21 10:21:56 +0530

    script-console-basics

---
+++ javascript-beginner-practice-questions/script-console-basics.js
@@ -0,0 +1,34 @@
+// Print "Hello JavaScript" in the console.
+console.log('log for General messages');
+console.log('Hello JavaScript');
+
+
+// Print your name, age, and city using one console.log().
+console.log('Gauarv Singh', 25, 'Lucknow');
+
+// Print a warning message using console.warn().
+console.warn('warn for Warnings')
+console.warn("This is a warning!");
+
+// Print an error message using console.error().
+console.error("error for Errors");
+console.error("error for Errors");
+
+
+// Use console.table() to display an array of 5 numbers.
+// Array
+const arr = [ "Array", 1, 2, 3, 4, 5]
+console.table(arr);
+
+// Array of Objects
+const user = [
+    {name: "Array of Objects", age: 3 },
+    {name: "Gaurav Singh", age: 27 },
+    {name: "Anuradha Singh", age: 25 }
+]
+console.table(user);
+
+
+
+
+
