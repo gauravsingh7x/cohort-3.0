24bc9bba152a18a16c5fbf90b34b2de0c10f4dde
Gaurav Singh <gaurav2115402-d@akgec.ac.in>
2026-06-01 17:42:18 +0530

    javascript-classes-live

--- javascript-classes/js-console/console.js
+++
@@ -1,4 +0,0 @@
-console.log("Normal message");
-console.warn("This is a warning");      // shown in yellow in browsers
-console.error("This is an error");      // shown in red
-console.table([1, 2, 3]);               // prints data as a table
--- javascript-classes/js-console/string.js
+++
@@ -1,6 +0,0 @@
-//A string is a sequence of characters wrapped in ' ', " ", or  .
-let s1 = 'Single quotes';
-let s2 = "Double quotes";
-let s3 = `Backticks (template literals)`;
-
-console.log(s1, s2, s3);
