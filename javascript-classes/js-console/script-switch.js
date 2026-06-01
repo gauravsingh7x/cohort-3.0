75ce5b2b149ac7d6eae84a69802a0bff8ce3a247
Gaurav Singh <gaurav2115402-d@akgec.ac.in>
2026-06-01 17:44:44 +0530

    javascript-classes-live

---
+++ javascript-classes/js-console/script-switch.js
@@ -0,0 +1,53 @@
+
+// while loop
+
+var w = 0;
+console.log('loop ke phle');
+while(w<5){
+    console.log('While Loop');
+    w++;
+}
+console.log('loop ke bad');
+
+
+/*
+var w=10;
+console.log('loop ke phle');
+while(w>0){
+    console.log(w);
+    w--;
+}
+console.log('loop ke bad');
+*/
+
+/*
+var num = Number(prompt('Enter Number'));
+var w=1;
+
+while(w<=num){
+    if(w%2 == 0){
+        console.log(w);
+        w++;
+    }
+}
+console.log('loop ke bad');
+*/
+
+
+// do-while loop
+/*
+do{
+    var pwd = prompt('Enter the password');
+}while(pwd!='123')
+console.log('Welcome!');
+
+
+//clear
+//  for loop
+for(var f=0; f<=10; f++){
+    console.log('for loop');
+}
+*/
+
+
+
